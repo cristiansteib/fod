2961218614b5efbc4669fe1044f19d64d3643f2b
cristian1992mas <steibkhriz@gmail.com>
2016-04-18 04:23:33 -0300

    update

--- Practica 2/2.pas
+++ Practica 2/2.pas
@@ -1,6 +1,17 @@
 {
    2-2.pas
- Author=Cristian   Steib
+ Author=Cristian Steib
+ * 
+ * 
+ * 
+   el archivo alumnos.txt debe contener
+    nombre
+    apellido
+    codigo materias_sin_final materias_con_final
+   
+   
+   el archivo detalle.txt debe contener
+	codigo materias_sin_final materias_con_final
    
 }
 
@@ -92,6 +103,7 @@ procedure actualizar_maestro(var m:archivoMaestro ;var d:archivoDetalle );
 		regm:alumnoM;
 		regd:alumnoD;
 	begin
+		clrscr;
 		reset(m);reset(d);
 		leer (d,regd);
 		read(m,regm);
@@ -114,14 +126,31 @@ procedure actualizar_maestro(var m:archivoMaestro ;var d:archivoDetalle );
 		
 		
 	end;
+	
+
+procedure punto_f (var m:archivoMaestro;var t:text);
+	var
+		reg:alumnoM;
+	begin
+		reset(m);
+		rewrite(t);
+		while (not EOF (m)) do begin
+			read(m,reg);
+			if (reg.materiasSinFinal>=4) then
+				writeln(t,reg.nombre,' ',reg.apellido,' ',reg.materiasSinFinal,' ',reg.materiasFinal);
+		end;
+		close(m);
+		close(t);
+	end;
 
 
 var
 	opcion:byte;
 	mae:archivoMaestro;
 	det:archivoDetalle;
-	detTxt,	maeTxt:text;
+	detTxt,	maeTxt, aTexto:text;
 	txtReporteAlum,txtReporteDet:text;
+	fileName:string;
 BEGIN
 	assign(mae,'maestro.bin');
 	assign(det,'detalle.bin');
@@ -136,7 +165,8 @@ BEGIN
 		writeln('2) Crear Detalle a partir de detalle.txt');
 		writeln('3) Crear reporte del archivo maestro a reporteAlumnos.txt');
 		writeln('4) Crear reporte del archivo detalle a reporteDetalle.txt');
-		writeln('5) Actualizar maestro');
+		writeln('5) Actualizar maestro.');
+		writeln('6) Informar alumnos con mas de 4 materias sin final.');
 		writeln('0) SALIR');
 		Writeln;
 		write('Ingrese N° de opcion: ');readln(opcion);
@@ -146,6 +176,12 @@ BEGIN
 			3:exportarMae(mae,txtReporteAlum);
 			4:exportarDet(det,txtReporteDet);
 			5:actualizar_maestro(mae,det);
+			6:begin
+				Writeln;
+				Write('Ingrese el nombre del archivo a generar');readln(fileName);
+				assign(aTexto,fileName+'.txt');
+				punto_f (mae,aTexto);
+			  end;
 		 end;
 		clrscr; 
 	until opcion=0;
