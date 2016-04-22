5ea7b7ac6495ee0f68e8e6bb505953b92c0e8170
cristiansteib <steibkhriz@gmail.com>
2016-04-22 01:26:22 -0300

    update

--- Practica 2/9/9.pas
+++ Practica 2/9/9.pas
@@ -11,6 +11,21 @@ type
 	   end;
 	   
 valor_de_hora=array [1..15] of real;
+archivo=file of empleado;
+
+
+procedure importar(var a:archivo;var t:text);
+	var 
+		reg:empleado;
+	begin
+		reset(t);rewrite(a);
+		while not eof(t) do begin
+			readln(t,reg.departamento,reg.division,reg.numero_empleado,reg.categoria,reg.cantidad_horas);
+			write(a,reg);
+		end;
+		close(a);close(t);
+	end;
+	
 
 
 procedure cargar_arreglo(var arreglo:valor_de_hora;var t:text);
@@ -26,14 +41,30 @@ procedure cargar_arreglo(var arreglo:valor_de_hora;var t:text);
 		close(t);
 	end;
 	
+procedure listar(var a:archivo;var arreglo:valor_de_hora);
+	
+	begin
+	end;
+	
+	
 
 var
 	fileName:string;
-	arch_arreglo:text;
+	arch_arreglo,text_arch_empleado:text;
+	arch_empleado:archivo;
 	arreglo:valor_de_hora;
 begin
 	Write('Nombre del archivo array: ');readln(fileName);
 	assign(arch_arreglo,fileName);
 	cargar_arreglo(arreglo,arch_arreglo);
+	
+	Write('Nombre del archivo empleado txt');readln(fileName);
+	assign(text_arch_empleado,fileName);
+	assign(arch_empleado,'empleados.bin');
+	
+	listar(arch_empleado,arreglo);
+	
+	
+	
 
 end.
